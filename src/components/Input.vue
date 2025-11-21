c8c2b9ed4d122949c69f3170fbf5a5c93cf20ae9
Jess Goodlett <45467320+jessgoodlett@users.noreply.github.com>
2025-11-20 21:40:04 -0600

    Updated styling to Input component

--- src/components/Input.vue
+++ src/components/Input.vue
@@ -2,31 +2,25 @@
   <div
     :class="[
       'Px-input',
-      {'Px-input--required': isRequired},
-      { 'Px-input--invalid': isInvalid }
+      {'Px-input--required': isRequired && !disabled && !readOnly},
+      {'Px-input--invalid': isInvalid},
+      {'Px-input--disabled': disabled}
     ]"
   >
     <div class="Px-input__label-wrapper">
       <label class="Px-input__label" :for="id || generateAttribute('id')">{{ label }}</label>
     </div>
     <div class="Px-input__container">
-      <div
-       :class="[
-          'Px-input__icon',
-          { 'Px-input__icon--left': iconLeft },
-        ]"
-      ></div>
        <input
         class="Px-input__input"
-         :id="id || generateAttribute('id')"
-         v-model="inputValue"
-         :required="isRequired"
-         :disabled="disabled"
-         :max="maxNumber"
-         :min="minNumber"
-         :placeholder="placeholder"
-         :readonly="readOnly"
-         :aria-invalid="isInvalid"
+        :id="id || generateAttribute('id')"
+        v-model="inputValue"
+        :required="isRequired && !disabled && !readOnly"
+        :disabled="disabled"
+        :placeholder="placeholder"
+        :readonly="readOnly"
+        :aria-invalid="isInvalid"
+        v-bind="$attrs"
        />
      </div>
     <div class="Px-input__message-wrapper">
@@ -42,13 +36,10 @@ interface Props {
   id?: string
   disabled?: boolean
   errorMessage?: string
-  iconLeft?: string
   isRequired?: boolean
   isInvalid?: boolean
   label?: string
   modelValue?: string | number
-  maxNumber?: number
-  minNumber?: number
   placeholder?: string
   readOnly?: boolean
 }
@@ -71,54 +62,114 @@ const generateAttribute = (attribute: string): string | undefined => {
 }
 </script>
 
-<style lang="scss" scoped>
+<style lang="scss">
+:root {
+  --px-input-primary-bg: #eeeeee;
+  --px-input-shadow: inset 3px 3px 4px #CACACB, inset -4px -4px 4px #f6f6f6;
+  --px-input-shadow-hover: inset 4px 4px 4px #CACACB, inset -4px -4px 4px #f6f6f6;
+  --px-input-shadow-active: inset 4px 4px 4px #CACACB, inset -4px -4px 4px #f6f6f6;
+  --px-input-shadow-disabled: inset 2px 2px 3px #e0e0e0, inset -2px -2px 3px #f8f8f8;
+  --px-input-text: #1B1B1B;
+  --px-input-text-disabled: #A0A0A0;
+  --px-input-text-placeholder: #6e6e6e;
+  --px-input-text-placeholder-hover: #4d4d4d;
+  --px-input-required: #DD1133;
+}
+
 .Px-input {
   display: flex;
   flex-direction: column;
   width: 100%;
-  gap: 8px;
+  gap: var(--px-space-100);
+  font-family: var(--px-font-family-body);
+  color: var(--px-input-text);
 
   .Px-input__input {
-    border: 1px solid var(--color-neutral-200);
-    background-color: var(--color-neutral-200);
-    padding: 12px 8px;
-    border-radius: 4px;
+    background-color: var(--px-input-primary-bg);
+    border: none;
+    padding: var(--px-space-150);
+    border-radius: var(--px-space-100);
     width: 100%;
-    font-size: 16px;
+    font-size: var(--px-space-200);
     letter-spacing: 1px;
     box-sizing: border-box;
+    box-shadow: var(--px-input-shadow);
+
+    &:hover:not(:disabled, :read-only) {
+      box-shadow: var(--px-input-shadow-hover);
+
+      &::placeholder {
+        color: var(--px-input-text-placeholder-hover);
+      }
+    }
+
+    &:active {
+      box-shadow: var(--px-input-shadow-active);
+    }
+
+    &:focus-visible {
+      outline: 2px solid rgba(0, 120, 255, 0.8);
+      outline-offset: 3px;
+    }
+
+    &:disabled {
+      box-shadow: var(--px-input-shadow-disabled);
+
+      &::placeholder {
+        color: var(--px-input-text-disabled);
+      }
+    }
+
+    &:read-only {
+      &::placeholder {
+        color: var(--px-input-text-disabled);
+      }
+    }
   }
 
   .Px-input__input::placeholder {
-    color: #6D7174;
-    font-size: 16px;
+    font-size: var(--px-font-size-body);
     letter-spacing: 1px;
-  }
-
-  .Px-input__input:focus-visible {
-    outline: none;
+    color: var(--px-input-text-placeholder);
   }
 
   .Px-input__label-wrapper,
   .Px-input__message-wrapper {
-    padding: 0 8px;
+    padding: var(--px-space-0) var(--px-space-050);
   }
 
   .Px-input__label {
-    font-size: 14px;
-    color: #6D7174;
+    font-size: var(--px-font-size-body-sm);
   }
 
   .Px-input__message-wrapper {
-    font-size: 12px;
-    color: #6D7174;
+    font-size: var(--px-font-size-caption);
+  }
+
+  &.Px-input--invalid,
+  &.Px-input--invalid:hover {
+    .Px-input__input,
+    .Px-input__input::placeholder {
+      color: var(--px-input-required);
+    }
+
+    .Px-input__message-wrapper {
+      color: var(--px-input-required);
+    }
   }
 }
+
 .Px-input--required {
   .Px-input__label::after {
     content: '*';
-    color: #DD1133;
-    margin-left: 4px;
+    color: var(--px-input-required);
+    margin-left: var(--px-space-050);
+  }
+}
+
+.Px-input--disabled {
+  .Px-input__label {
+    color: var(--px-input-text-disabled);
   }
 }
 </style>
