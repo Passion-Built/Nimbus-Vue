38241fdf6763e2b334aa8cab942c5483f23d9109
Jess Goodlett <45467320+jessgoodlett@users.noreply.github.com>
2025-11-24 22:17:24 -0600

    Restructure base tokens

--- src/main.js
+++ src/main.js
@@ -1,7 +1,6 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-import './styles/tokens.css';
-import './styles/fonts.css';
+import './styles/index.css';
 
 const app = createApp(App)
 
