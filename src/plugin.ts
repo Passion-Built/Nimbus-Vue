87d9e1071b7b87d4f6bbd18a45650843d5ccb3d0
Jess Goodlett <45467320+jessgoodlett@users.noreply.github.com>
2025-12-04 20:59:58 -0600

    Remove theming file and udpated plugin

--- src/plugin.ts
+++ src/plugin.ts
@@ -1,26 +1,15 @@
 // src/plugin.ts
 import type { App } from 'vue'
 import * as components from './components'
-import { applyTheme } from './styles/tokens/color/themes/applyTheme'
 
 import './styles/index.css'
 
-export interface PxUIOptions {
-  theme?: Record<string, string>
-  disableGlobalRegistration?: boolean
-}
-
 const PxUI = {
-  install(app: App, options: PxUIOptions = {}) {
+  install(app: App) {
     // Register all components
     Object.entries(components).forEach(([name, component]) => {
       app.component(name, component)
     })
-
-    // Apply theme overrides
-    if (options.theme) {
-      applyTheme(options.theme)
-    }
   }
 }
 
--- src/styles/tokens/color/themes/applyTheme.ts
+++
@@ -1,7 +0,0 @@
-export function applyTheme(theme: Record<string, string>) {
-  const root = document.documentElement
-
-  for (const [key, value] of Object.entries(theme)) {
-    root.style.setProperty(`--px-${key}`, value)
-  }
-}
