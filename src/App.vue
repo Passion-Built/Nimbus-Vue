5555f3103fc28db69c0b357ed94a2bc48b88d27d
Jess Goodlett <45467320+jessgoodlett@users.noreply.github.com>
2026-06-16 21:56:33 -0500

    Added Image component and stories

--- src/App.vue
+++ src/App.vue
@@ -54,6 +54,8 @@
     <PxLink url="#" navigation>Test Link</PxLink>
     <PxLink url="#" navigation current>Test Link (current)</PxLink>
     <PxLink url="https://www.google.com" external>Test Link</PxLink>
+    <PxImage src="https://picsum.photos/240/160" alt="Sample image" width="240" height="160" fit="cover" />
+    <PxImage src="https://picsum.photos/240/160?2" alt="Elevated sample image" width="240" height="160" fit="cover" elevated />
     <PxIconContainer><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 640 640"><path d="M32 400C32 479.5 96.5 544 176 544L480 544C550.7 544 608 486.7 608 416C608 364.4 577.5 319.9 533.5 299.7C540.2 286.6 544 271.7 544 256C544 203 501 160 448 160C430.3 160 413.8 164.8 399.6 173.1C375.5 127.3 327.4 96 272 96C192.5 96 128 160.5 128 240C128 248 128.7 255.9 129.9 263.5C73 282.7 32 336.6 32 400z"/></svg></PxIconContainer>
     <PxIconContainer isClickable><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 640 640"><path d="M32 400C32 479.5 96.5 544 176 544L480 544C550.7 544 608 486.7 608 416C608 364.4 577.5 319.9 533.5 299.7C540.2 286.6 544 271.7 544 256C544 203 501 160 448 160C430.3 160 413.8 164.8 399.6 173.1C375.5 127.3 327.4 96 272 96C192.5 96 128 160.5 128 240C128 248 128.7 255.9 129.9 263.5C73 282.7 32 336.6 32 400z"/></svg></PxIconContainer>
     <PxInput
---
+++ src/components/PxImage.vue
@@ -0,0 +1,43 @@
+<template>
+  <img
+    :class="['Px-image', { 'Px-image--elevated': elevated }]"
+    :src="src"
+    :alt="alt"
+    :width="width"
+    :height="height"
+    :loading="loading"
+    :style="{
+      objectFit: fit,
+      objectPosition: position,
+    }"
+  />
+</template>
+
+<script setup lang="ts">
+interface Props {
+  src: string
+  alt: string
+  width?: string | number
+  height?: string | number
+  fit?: 'cover' | 'contain' | 'fill' | 'none' | 'scale-down'
+  position?: string
+  loading?: 'lazy' | 'eager'
+  elevated?: boolean
+}
+
+withDefaults(defineProps<Props>(), {
+  loading: 'lazy',
+})
+</script>
+
+<style scoped>
+.Px-image {
+  display: block;
+  max-width: 100%;
+  border-radius: var(--px-image-radius);
+}
+
+.Px-image--elevated {
+  box-shadow: var(--px-image-shadow);
+}
+</style>
