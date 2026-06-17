aa0ec5c6bf2c3cdc9915c3d966874c2c91f8048d
Jess Goodlett <45467320+jessgoodlett@users.noreply.github.com>
2026-06-17 05:25:55 -0500

    Added as prop to Box for semantic html render (#24)

--- src/stories/Box.stories.js
+++ src/stories/Box.stories.js
@@ -9,6 +9,11 @@ export default {
       control: 'text',
       description: 'The default text content of the box.',
     },
+    as: {
+      control: { type: 'select' },
+      options: ['div', 'section', 'article', 'aside', 'nav', 'main', 'header', 'footer'],
+      description: 'The semantic HTML element to render as. Defaults to `div`.',
+    },
     clickable: {
       control: 'boolean',
       description: 'Applies a pointer cursor and inset shadow on hover to indicate interactivity.',
@@ -17,12 +22,12 @@ export default {
       control: 'boolean',
       description: 'Stretches the box to fill its container width.',
     },
-    render: (args) => ({
-      components: { PxBox },
-      setup() { return { args } },
-      template: '<PxBox v-bind="args">{{ args.default }}</PxBox>',
-    }),
   },
+  render: (args) => ({
+    components: { PxBox },
+    setup() { return { args } },
+    template: '<PxBox v-bind="args">{{ args.default }}</PxBox>',
+  }),
   args: {
     default: 'Icing lemon drops marzipan candy apple pie lollipop. I love I love icing shortbread gummies carrot cake. Candy tiramisu cookie brownie jelly-o sweet sugar plum lollipop. Fruitcake shortbread chocolate bar pie gummi bears fruitcake icing cake caramels.',
     fullWidth: false,
